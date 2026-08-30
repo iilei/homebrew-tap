84843875ebaeed26df2d9fdc695d018423a3c71e
iilei • jochen preusche <922226+iilei@users.noreply.github.com>
2026-08-31 01:05:53 +0200

    band-aid: use correct v0.1.0 version and real linux_x86_64 checksum, mark others as TODO

--- Formula/lane-keeper.rb
+++ Formula/lane-keeper.rb
@@ -2,11 +2,11 @@ class LaneKeeper < Formula
   desc "Repository readiness checks and read-only workflow helpers"
   homepage "https://github.com/iilei/lane-keeper"
   license "MIT"
-  version "0.1.1"
+  version "0.1.0"
 
   on_macos do
     url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_darwin_universal2.tar.gz"
-    sha256 "4bf0da0760603fefac48cc4d8ea950e7d95e7a7c4669e7114e28bc29b9f55a47"
+    sha256 "TODO_REPLACE_DARWIN_UNIVERSAL2"
 
     def install
       bin.install "lane-keeper"
@@ -17,7 +17,7 @@ class LaneKeeper < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_linux_x86_64.tar.gz"
-      sha256 "c8784149f2f214652e6e002e7934f9ce9e2a5284ead7376476f4fdfbff987411"
+      sha256 "69c0e045b0ae0a4e8119e94b5589cbf96acec79e269adacd21cf956aac56efe1"
 
       define_method(:install) do
         bin.install "lane-keeper"
@@ -27,7 +27,7 @@ class LaneKeeper < Formula
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_linux_arm64.tar.gz"
-      sha256 "173ef05d71a9f73843894b3bec2265e7b8f0e3dd965e609aa14441d77686e2b9"
+      sha256 "TODO_REPLACE_LINUX_ARM64"
 
       define_method(:install) do
         bin.install "lane-keeper"
@@ -37,7 +37,7 @@ class LaneKeeper < Formula
 
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
       url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_linux_armv7.tar.gz"
-      sha256 "6f35527ee8266e9e7a3dae4be39071affc1931183e4a4cea5e6cc4e8d5cd5230"
+      sha256 "TODO_REPLACE_LINUX_ARMV7"
 
       define_method(:install) do
         bin.install "lane-keeper"
