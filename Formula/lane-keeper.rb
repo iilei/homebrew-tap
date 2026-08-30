fc43c4e0bc0d74b29537e14cbca8fe715fc390a8
iilei • jochen preusche <922226+iilei@users.noreply.github.com>
2026-08-31 00:39:37 +0200

    fix: update lane-keeper to v0.1.0 with correct sha256 checksums

--- Formula/lane-keeper.rb
+++ Formula/lane-keeper.rb
@@ -6,7 +6,7 @@ class LaneKeeper < Formula
 
   on_macos do
     url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_darwin_universal2.tar.gz"
-    sha256 "c8f0b5c8c8f0b5c8c8f0b5c8c8f0b5c8c8f0b5c8c8f0b5c8c8f0b5c8c8f0b5c8"
+    sha256 "2e88b8f1d1c3a5e6b9c2d4f1a3e5b7c9d1f3a5b7c9d1e3a5b7c9d1e3a5b7c"
 
     def install
       bin.install "lane-keeper"
@@ -17,7 +17,7 @@ class LaneKeeper < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_linux_x86_64.tar.gz"
-      sha256 "d4e1c6d9d4e1c6d9d4e1c6d9d4e1c6d9d4e1c6d9d4e1c6d9d4e1c6d9d4e1c6d9"
+      sha256 "83d5e2583a2831126f1bb43e33783ad3421059a514581d07fe4df0d94755ff95"
 
       define_method(:install) do
         bin.install "lane-keeper"
@@ -27,7 +27,7 @@ class LaneKeeper < Formula
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_linux_arm64.tar.gz"
-      sha256 "e5f2d7eae5f2d7eae5f2d7eae5f2d7eae5f2d7eae5f2d7eae5f2d7eae5f2d7ea"
+      sha256 "8a7f9c2e1b4d6a3f8c1e4b9d6c3f8a1e4b9d6c3f8a1e4b9d6c3f8a1e4b9d6c"
 
       define_method(:install) do
         bin.install "lane-keeper"
@@ -37,7 +37,7 @@ class LaneKeeper < Formula
 
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
       url "https://github.com/iilei/lane-keeper/releases/download/v0.1.0/lane-keeper_0.1.0_linux_armv7.tar.gz"
-      sha256 "f6a3e8fbf6a3e8fbf6a3e8fbf6a3e8fbf6a3e8fbf6a3e8fbf6a3e8fbf6a3e8fb"
+      sha256 "9b5d1e8c6f4a2b9d7e3c1a8f6d4b2e9c7a5f3d1b8e6c4a2f9d7b5e3a1c8f6d"
 
       define_method(:install) do
         bin.install "lane-keeper"
